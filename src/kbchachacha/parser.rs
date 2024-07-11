1470d2f3ff38c929391a9d4bd9446b9112b7e5cb
kernel <kernel@debian.host>
2024-07-11 14:51:53 +1000

    s

--- src/kbchachacha/parser.rs
+++ src/kbchachacha/parser.rs
@@ -14,17 +14,19 @@ pub struct Todo {
 pub async fn parse() -> Result<Vec<Todo>, reqwest::Error> {
     let marker_url = String::from("https://jsonplaceholder.typicode.com/todos?userId=1");
     match reqwest::Client::new().get(&marker_url).send().await {
+        /* -- successfull respose  --*/
         Ok(v) => match v.json().await {
+            /* -- Successfull decode  --*/
             Ok(s) => return Ok(s),
+            /* -- Error decode  --*/
             Err(decode_error) => {
-                eprintln!("Error decoding maker: {}", { &decode_error });
+                eprintln!("Error decoding maker: {decode_error}");
                 return Err(decode_error);
             }
         },
+        /* -- Error respose  --*/
         Err(request_error) => {
-            eprintln!("Error requesting maker: {},\n {}", { &marker_url }, {
-                &request_error
-            });
+            eprintln!("Error requesting maker: {marker_url},\n {request_error}");
             return Err(request_error);
         }
     };
