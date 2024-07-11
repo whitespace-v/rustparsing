9cacdd8fd4c058f986e34012086fcc2a20439572
kernel <kernel@debian.host>
2024-07-11 14:41:30 +1000

    s

--- src/kbchachacha/parser.rs
+++ src/kbchachacha/parser.rs
@@ -1,9 +1,27 @@
 #![warn(clippy::all, clippy::pedantic)]
-use crate::kbchachacha::*;
-use tokio;
-#[tokio::main]
-pub async fn parse() {
-    // parse makers:
-    let r = maker::parse_maker();
-    println!("{r:?}")
+use reqwest;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Todo {
+    user_id: i32,
+    id: Option<i32>,
+    title: String,
+    completed: bool,
+}
+
+pub async fn parse() -> Vec<Todo> {
+    let marker_url = String::from("https://jsonplaceholder.typicode.com/todos?userId=1");
+    let mut todo: Vec<Todo> = vec![];
+    match reqwest::Client::new().get(&marker_url).send().await {
+        Ok(v) => match v.json().await {
+            Ok(s) => todo = s,
+            Err(decode_error) => eprintln!("Error decoding maker: {decode_error}"),
+        },
+        Err(request_error) => eprintln!("Error requesting maker: {},\n {}", { &marker_url }, {
+            &request_error
+        }),
+    };
+    todo
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,8 @@
 #![warn(clippy::all, clippy::pedantic)]
-use reqwest::{self, Client};
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct Todo {
-    user_id: i32,
-    id: Option<i32>,
-    title: String,
-    completed: bool,
-}
+mod kbchachacha;
 
 #[tokio::main]
-pub async fn main() {
-    let maker = parse().await;
+async fn main() {
+    let maker = kbchachacha::parser::parse().await;
     println!("{maker:#?}")
-}
-
-async fn parse() -> Vec<Todo> {
-    let marker_url = String::from("https://jsonplaceholder.typicode.com/todos?userId=1");
-    let mut todo: Vec<Todo> = vec![];
-    match reqwest::Client::new().get(&marker_url).send().await {
-        Ok(v) => match v.json().await {
-            Ok(s) => todo = s,
-            Err(decode_error) => eprintln!("Error decoding maker: {decode_error}"),
-        },
-        Err(request_error) => eprintln!("Error requesting maker: {},\n {}", { &marker_url }, {
-            &request_error
-        }),
-    };
-    todo
 }
